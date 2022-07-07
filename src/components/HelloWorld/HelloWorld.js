df97b17a7578d24b86dd06d8abc5683c7d81878f
alieslamdoost <alieslamdooost@gmail.com>
2022-07-07 14:44:12 +0430

    modifyApp

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class HelloWorld extends Component {
+  render() {
+    return <div>"HelloWorld" </div>;
+  }
+}
+
+export default HelloWorld;
---
+++ src/components/User/User.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class User extends Component{
+
+    super()
+
+constructor{
+const user =[
+{
+    name:"mehran",
+    id:1
+},
+{
+    name:"ali",
+    id:2
+},
+{
+    name:"amirhosein",
+    id:3
+},
+{
+    name:"mohammad",
+    id:4
+}
+
+]
+
+}
+
+
+}
